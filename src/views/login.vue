638c56884f2b5f675130827bc0262e4632efa8fd
shenjizhe <shenjizhe@bfec.cn>
2023-02-16 18:05:38 +0800

    添加了很多的图标

--- src/views/login.vue
+++ src/views/login.vue
@@ -2,7 +2,7 @@
   <div class="login" :style="'background-image:url('+ Background +');'">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" label-position="left" label-width="0px" class="login-form">
       <h3 class="title">
-        ELADMIN 后台管理系统
+        代码工厂
       </h3>
       <el-form-item prop="username">
         <el-input v-model="loginForm.username" type="text" auto-complete="off" placeholder="账号">
@@ -47,7 +47,8 @@ import Config from '@/settings'
 import { getCodeImg } from '@/api/login'
 import Cookies from 'js-cookie'
 import qs from 'qs'
-import Background from '@/assets/images/background.webp'
+// import Background from '@/assets/images/background.webp'
+import Background from '@/assets/images/code.png'
 export default {
   name: 'Login',
   data() {
