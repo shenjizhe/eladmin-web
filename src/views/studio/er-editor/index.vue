47c1c8bc21120d3b0a95e9e8ac92098b5a41abae
shenjizhe <shenjizhe@bfec.cn>
2023-02-20 13:38:50 +0800

    添加了实体相关的逻辑和页面

---
+++ src/views/studio/er-editor/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+<script src="./index.js"/>
+<style scoped src="./index.css"/>
+
