13370610fc91d52661af8d663d4b18abc8d8985d
shenjizhe <shenjizhe@bfec.cn>
2023-02-24 10:27:21 +0800

    工具栏拖拽控件

--- src/views/studio/er-editor/_er-editor.js
+++ src/views/studio/er-editor/_er-editor.js
@@ -1,35 +1,23 @@
 import draggable from 'vuedraggable'
 import EREntity from '@/components/EREntity'
-// import crudEntity from '@/api/entity'
-// import crudDomain from '@/api/domain'
-// import CRUD, { presenter, header, form, crud } from '@crud/crud'
 import rrOperation from '@crud/RR.operation'
 import crudOperation from '@crud/CRUD.operation'
 import udOperation from '@crud/UD.operation'
 import pagination from '@crud/Pagination'
 
-// const defaultForm = { id: null, domainId: null, name: null, title: null, comment: null, show: null, groupId: null }
 export default {
   name: 'ErEditor',
   components: { EREntity, draggable, pagination, crudOperation, rrOperation, udOperation },
-  // mixins: [presenter(), header(), form(defaultForm), crud()],
   dicts: ['entity_types', 'db_types'],
   props: {},
-
-  // cruds() {
-  //   return CRUD({ tag: 'entity', title: '实体', url: 'api/entity', idField: 'id', sort: 'id,desc', crudMethod: { ...crudEntity }})
-  // },
-  //
-  // crudsDomain() {
-  //   return CRUD({ title: '领域建模', url: 'api/domain', idField: 'id', sort: 'id,desc', crudMethod: { ...crudDomain }})
-  // },
 
   data() {
     return {
       domain: {
         id: 0,
         name: '',
-        comment: ''
+        comment: '',
+        show: false
       },
       toolEntity: [
         {
@@ -66,9 +54,6 @@ export default {
   },
 
   methods: {
-    // [CRUD.HOOK.beforeRefresh]() {
-    //   return true
-    // },
     onStart(e) {
       console.log(e)
     },
