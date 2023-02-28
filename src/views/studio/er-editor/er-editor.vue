b1f8e3b75714a0edfd60490ea1e5c014e8dc6efe
shenjizhe <shenjizhe@bfec.cn>
2023-02-28 15:05:04 +0800

    tab更换字体颜色

--- src/views/studio/er-editor/er-editor.vue
+++ src/views/studio/er-editor/er-editor.vue
@@ -1,58 +1,66 @@
 <template>
   <el-container class="blackStyle">
     <el-aside>
-      <el-collapse accordion>
-        <el-collapse-item title="领域信息" name="domain-info">
-          <template slot="title">
-            <i class="header-icon el-icon-info">领域信息</i>
-          </template>
-          <el-form v-model="domain" label-width="50px" size="small">
-            <el-form-item label="名称">
-              <el-input v-model="domain.name" />
-            </el-form-item>
-            <el-form-item label="描述">
-              <el-input v-model="domain.comment" type="textarea" />
-            </el-form-item>
-            <el-form-item label="启用">
-              <el-switch v-model="domain.show" />
-            </el-form-item>
-          </el-form>
-        </el-collapse-item>
-        <el-collapse-item title="工具控件" name="domain-tool">
-          <draggable v-model="toolEntity" class="entity-tool" chosen-class="chosen" :group="{name: 'entity-area-group', pull: 'clone', put: false}" sort="false" animation="300">
-            <EREntity v-for="item in toolEntity" :id="item.id" :key="item.id" :model="item" :type="item.type" :title="item.title" />
-          </draggable>
-        </el-collapse-item>
-        <el-collapse-item title="市场领域" name="domain-market">
-          <el-input placeholder="请输入关键字">
-            <el-button slot="append" icon="el-icon-search" />
-          </el-input>
-          <el-table
-            style="width: 100%"
-            stripe
-          >
-            <el-table-column
-              label="名称"
-              width="80px"
-            />
-            <el-table-column
-              prop="name"
-              label="描述"
-            />
-          </el-table>
-          <el-form label-width="50px" size="small">
-            <el-form-item label="名称">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="描述">
-              <el-input />
-            </el-form-item>
-            <el-form-item label="用途">
-              <el-input type="textarea" />
-            </el-form-item>
-          </el-form>
-        </el-collapse-item>
-      </el-collapse>
+      <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+        <el-tab-pane label="领域管理" name="first">
+          <el-collapse>
+            <el-collapse-item title="领域信息" name="domain-info">
+              <template slot="title">
+                <i class="header-icon el-icon-info">领域信息</i>
+              </template>
+              <el-form v-model="domain" label-width="50px" size="small">
+                <el-form-item label="名称">
+                  <el-input v-model="domain.name" />
+                </el-form-item>
+                <el-form-item label="描述">
+                  <el-input v-model="domain.comment" type="textarea" />
+                </el-form-item>
+                <el-form-item label="启用">
+                  <el-switch v-model="domain.show" />
+                </el-form-item>
+              </el-form>
+            </el-collapse-item>
+            <el-collapse-item title="工具控件" name="domain-tool">
+              <draggable v-model="toolEntity" class="entity-tool" chosen-class="chosen" :group="{name: 'entity-area-group', pull: 'clone', put: false}" sort="false" animation="300">
+                <EREntity v-for="item in toolEntity" :id="item.id" :key="item.id" :model="item" :type="item.type" :title="item.title" />
+              </draggable>
+            </el-collapse-item>
+          </el-collapse>
+        </el-tab-pane>
+        <el-tab-pane label="实体市场" name="second">
+          <el-collapse>
+            <el-collapse-item title="市场领域" name="domain-market">
+              <el-input placeholder="请输入关键字">
+                <el-button slot="append" icon="el-icon-search" />
+              </el-input>
+              <el-table
+                style="width: 100%"
+                stripe
+              >
+                <el-table-column
+                  label="名称"
+                  width="80px"
+                />
+                <el-table-column
+                  prop="name"
+                  label="描述"
+                />
+              </el-table>
+              <el-form label-width="50px" size="small">
+                <el-form-item label="名称">
+                  <el-input />
+                </el-form-item>
+                <el-form-item label="描述">
+                  <el-input />
+                </el-form-item>
+                <el-form-item label="用途">
+                  <el-input type="textarea" />
+                </el-form-item>
+              </el-form>
+            </el-collapse-item>
+          </el-collapse>
+        </el-tab-pane>
+      </el-tabs>
     </el-aside>
     <el-main>
       <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
@@ -68,7 +76,7 @@
       <pagination />
     </el-main>
     <el-aside>
-      <el-collapse accordion>
+      <el-collapse>
         <el-collapse-item title="实体信息" name="1">
           <el-form label-width="50px" size="small">
             <el-form-item label="名称">
