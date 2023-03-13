6cd771fbdace25cacbeee7e1ad686f4ebc7a01cd
shenjizhe <shenjizhe@bfec.cn>
2023-03-13 09:46:59 +0800

    准备升级到 element plus vue3

---
+++ src/views/studio/template-ide/template-ide.vue
@@ -0,0 +1,8 @@
+<!--<template>-->
+<!--    <div class="template-ide">-->
+<!--      <textarea ref="mycode" class="textarea" v-model="textarea"></textarea>-->
+<!--    </div>-->
+<!--</template>-->
+
+<!--<script src="src/views/studio/template-ide/_template-ide.ts" lang="ts"></script>-->
+<!--<style src="./_template-ide.scss" lang="scss" scoped></style>-->
