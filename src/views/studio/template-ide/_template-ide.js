5e732b44d941fa662d4ac3cdb046e7624f0fb03d
shenjizhe <shenjizhe@bfec.cn>
2023-03-27 20:01:55 +0800

    1. 自动补全增加图标

--- src/views/studio/template-ide/_template-ide.js
+++ src/views/studio/template-ide/_template-ide.js
@@ -63,6 +63,27 @@ function printVueLog(msg) {
   }
 }
 
+class HtmlText {
+  get iconText() {
+    return this._icon
+  }
+
+  setIcon(type) {
+    const arr = [
+      '<i class="el-icon-s-cooperation"></i>',
+      '<i class="el-icon-menu"></i>',
+      '<i class="el-icon-share"></i>',
+      '<i class="el-icon-s-data"></i>'
+    ]
+
+    return arr[type]
+  }
+
+  constructor(cm) {
+    this._icon = this.setIcon(cm.type)
+  }
+}
+
 export default {
   name: 'TemplateIde',
   components: { MyForm, pagination, crudOperation, rrOperation, udOperation },
@@ -506,7 +527,18 @@ export default {
     },
 
     getContextList() {
-      return this.Crud.context.data.map(cm => cm.key)
+      return this.Crud.context.data.map(cm => {
+        return {
+          text: cm.key,
+          displayText: cm.content,
+          render: (element, self, data) => {
+            // console.log(element,self ,data)
+            // element.innerText = cm.content
+            var htmlText = new HtmlText(cm)
+            element.innerHTML = '<div style="width: 200px">' + htmlText.iconText + '<el-input readonly >' + cm.content + '</el-input></div>'
+          }
+        }
+      })
     },
 
     setCodeContent(block, val) {
