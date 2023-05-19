59c30267b0cf6f27eafd3f61676c2f0babfcb84e
shenjizhe <shenjizhe@bfec.cn>
2023-05-19 19:52:07 +0800

    格式化了日期

--- src/views/financial/statics/_index.js
+++ src/views/financial/statics/_index.js
@@ -70,7 +70,12 @@ export default {
       //   return ''
       // }
       if (date) {
-        return date
+        const _date = new Date(date)
+        const year = _date.getFullYear() // 获取当前年份
+        const month = String(_date.getMonth() + 1).padStart(2, '0') // 获取当前月份，并将其前导零，使其成为两位数字
+        const day = String(_date.getDate()).padStart(2, '0') // 获取当前天数，并将其前导零，使其成为两位数字
+        const formattedDate = `${year}-${month}-${day}` // 将年、月、日组合成格式化后的日期字符串
+        return formattedDate
       } else {
         return ''
       }
