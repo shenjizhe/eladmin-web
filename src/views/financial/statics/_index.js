5fb107c10105ae1e4c6401f54c161a00e0a107f8
shenjizhe <shenjizhe@bfec.cn>
2023-05-19 19:46:04 +0800

    修改了枚举类型对应的文本，在 el-select 增加了 *1的修正

--- src/views/financial/statics/_index.js
+++ src/views/financial/statics/_index.js
@@ -1,13 +1,20 @@
-
 import { StockStaticHelper } from '@/api/financial/StockStaticHelper'
 import { StockStatics } from '@/api/financial/StockStatics'
+
 export default {
   name: 'StaticComponent',
   props: {},
 
   data() {
     return {
-      statics: {}
+      statics: {
+        stock: {
+          name: '',
+          code: ''
+        },
+        analysis: {},
+        level: {}
+      }
     }
   },
 
@@ -49,6 +56,24 @@ export default {
             type: 'fail'
           })
         })
+    },
+
+    formatDate(date) {
+      // if (date != null) {
+      //   const year = date.getFullYear() // 获取当前年份
+      //   const month = String(date.getMonth() + 1).padStart(2, '0') // 获取当前月份，并将其前导零，使其成为两位数字
+      //   const day = String(date.getDate()).padStart(2, '0') // 获取当前天数，并将其前导零，使其成为两位数字
+      //
+      //   const formattedDate = `${year}-${month}-${day}` // 将年、月、日组合成格式化后的日期字符串
+      //   return formattedDate
+      // } else {
+      //   return ''
+      // }
+      if (date) {
+        return date
+      } else {
+        return ''
+      }
     }
   }
 }
--- src/views/financial/statics/index.vue
+++ src/views/financial/statics/index.vue
@@ -2,20 +2,50 @@
   <el-container class="statics-container">
     <el-aside class="left-container">
       <el-card class="box-card">
-        <div slot="header">
-          <span>卡片名称</span>
-          <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
-        </div>
         <!--          <div v-for="o in 4" :key="o" class="text item">-->
         <!--            {{'列表内容 ' + o }}-->
-
+        <el-row>
+          <el-col :x-s="24" :sm="12" class="infoHead">{{ statics.stock.name }}</el-col>
+          <el-col :x-s="24" :sm="12" class="infoHead">{{ statics.stock.code }}</el-col>
+        </el-row>
+        <el-row>
+          <el-col :x-s="24" :sm="12">持仓数量</el-col>
+          <el-col :x-s="24" :sm="12">{{ statics.holdCount }}</el-col>
+        </el-row>
+        <el-row>
+          <el-col :x-s="24" :sm="12">持有单价</el-col>
+          <el-col :x-s="24" :sm="12">{{ statics.holdPriceAvg }}</el-col>
+        </el-row>
+        <el-row>
+          <el-col :x-s="24" :sm="12">平均时期</el-col>
+          <el-col :x-s="24" :sm="12">{{ formatDate(statics.holdDateAvg) }}</el-col>
+        </el-row>
+        <el-row>
+          <el-col :x-s="24" :sm="12">持股总价</el-col>
+          <el-col :x-s="24" :sm="12">{{ statics.holdPriceTotal }}</el-col>
+        </el-row>
+        <el-row>
+          <el-col :x-s="24" :sm="12" class="infoHead">市场平均</el-col>
+          <el-col :x-s="24" :sm="12" class="infoHead">{{ statics.analysis.priceAvg }}</el-col>
+        </el-row>
+        <el-row>
+          <el-col :x-s="24" :sm="12">参考价格</el-col>
+          <el-col :x-s="24" :sm="12">{{ statics.analysis.priceLow }} - {{ statics.analysis.priceHigh }}</el-col>
+        </el-row>
+        <el-row>
+          <el-col :x-s="24" :sm="12">参考 90</el-col>
+          <el-col :x-s="24" :sm="12">{{ statics.analysis.priceLow90 }} - {{ statics.analysis.priceHigh90 }}</el-col>
+        </el-row>
+        <el-row>
+          <el-col :x-s="24" :sm="12">参考 70</el-col>
+          <el-col :x-s="24" :sm="12">{{ statics.analysis.priceLow70 }} - {{ statics.analysis.priceHigh70 }}</el-col>
+        </el-row>
       </el-card>
     </el-aside>
     <el-main class="center-container" />
     <el-aside class="right-container" />
   </el-container>
 </template>
-
 <script lang="js" src="./_index.js">
 </script>
 
