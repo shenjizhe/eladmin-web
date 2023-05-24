b18766c64af55692f74c79f0e77f470173d0fcfb
shenjizhe <shenjizhe@bfec.cn>
2023-05-24 19:19:08 +0800

    1. 增加了词根 morpheme 工程和页面

--- src/views/morpheme/morpheme/index.vue
+++ src/views/morpheme/morpheme/index.vue
@@ -8,10 +8,10 @@
         <el-input v-model="query.id" clearable placeholder="主键" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
         <label class="el-form-item-label">字典ID</label>
         <el-input v-model="query.dictionaryId" clearable placeholder="字典ID" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
-        <label class="el-form-item-label">词素类型（1-词根、2-词缀,3-连接词）</label>
-        <el-input v-model="query.type" clearable placeholder="词素类型（1-词根、2-词缀,3-连接词）" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
-        <label class="el-form-item-label">词源(L,GK)</label>
-        <el-input v-model="query.source" clearable placeholder="词源(L,GK)" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <label class="el-form-item-label">词素类型</label>
+        <el-input v-model="query.type" clearable placeholder="词素类型" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <label class="el-form-item-label">词源</label>
+        <el-input v-model="query.source" clearable placeholder="词源" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
         <rrOperation :crud="crud" />
       </div>
       <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
@@ -22,13 +22,13 @@
           <el-form-item label="字典ID" prop="dictionaryId">
             <el-input v-model="form.dictionaryId" style="width: 95%;" />
           </el-form-item>
-          <el-form-item label="编号(1)" prop="number">
+          <el-form-item label="编号" prop="number">
             <el-input-number v-model="form.number" />
           </el-form-item>
-          <el-form-item label="词素文本（ag,act）" prop="text">
+          <el-form-item label="词素文本" prop="text">
             <el-input v-model="form.text" style="width: 95%;" />
           </el-form-item>
-          <el-form-item label="词素类型（1-词根、2-词缀,3-连接词）" prop="type">
+          <el-form-item label="词素类型" prop="type">
             <el-select v-model="form.type" filterable placeholder="请选择">
               <el-option
                 v-for="item in dict.morpheme_type"
@@ -38,7 +38,7 @@
               />
             </el-select>
           </el-form-item>
-          <el-form-item label="词源(L,GK)" prop="source">
+          <el-form-item label="词源" prop="source">
             <el-select v-model="form.source" filterable placeholder="请选择">
               <el-option
                 v-for="item in dict.morpheme_source"
@@ -68,21 +68,20 @@
         <el-table-column type="selection" width="55" />
         <el-table-column prop="id" label="主键" />
         <el-table-column prop="dictionaryId" label="字典ID" />
-        <el-table-column prop="number" label="编号(1)" />
-        <el-table-column prop="text" label="词素文本（ag,act）" />
-        <el-table-column prop="type" label="词素类型（1-词根、2-词缀,3-连接词）">
+        <el-table-column prop="number" label="编号" />
+        <el-table-column prop="text" label="词素文本" />
+        <el-table-column prop="type" label="词素类型">
           <template slot-scope="scope">
             {{ dict.label.morpheme_type[scope.row.type] }}
           </template>
         </el-table-column>
-        <el-table-column prop="source" label="词源(L,GK)">
+        <el-table-column prop="source" label="词源">
           <template slot-scope="scope">
             {{ dict.label.morpheme_source[scope.row.source] }}
           </template>
         </el-table-column>
         <el-table-column prop="meaningChinese" label="中文含义" />
         <el-table-column prop="meaningEnglish" label="英文含义" />
-        <el-table-column prop="description" label="说明" />
         <el-table-column v-if="checkPer(['admin','morpheme:edit','morpheme:del'])" label="操作" width="150px" align="center">
           <template slot-scope="scope">
             <udOperation
@@ -130,16 +129,16 @@ export default {
           { required: true, message: '字典ID不能为空', trigger: 'blur' }
         ],
         number: [
-          { required: true, message: '编号(1)不能为空', trigger: 'blur' }
+          { required: true, message: '编号不能为空', trigger: 'blur' }
         ],
         text: [
-          { required: true, message: '词素文本（ag,act）不能为空', trigger: 'blur' }
+          { required: true, message: '词素文本不能为空', trigger: 'blur' }
         ],
         type: [
-          { required: true, message: '词素类型（1-词根、2-词缀,3-连接词）不能为空', trigger: 'blur' }
+          { required: true, message: '词素类型不能为空', trigger: 'blur' }
         ],
         source: [
-          { required: true, message: '词源(L,GK)不能为空', trigger: 'blur' }
+          { required: true, message: '词源不能为空', trigger: 'blur' }
         ],
         meaningChinese: [
           { required: true, message: '中文含义不能为空', trigger: 'blur' }
@@ -151,8 +150,8 @@ export default {
       queryTypeOptions: [
         { key: 'id', display_name: '主键' },
         { key: 'dictionaryId', display_name: '字典ID' },
-        { key: 'type', display_name: '词素类型（1-词根、2-词缀,3-连接词）' },
-        { key: 'source', display_name: '词源(L,GK)' }
+        { key: 'type', display_name: '词素类型' },
+        { key: 'source', display_name: '词源' }
       ]
 
     }
